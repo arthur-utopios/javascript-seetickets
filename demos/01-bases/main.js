e9989a338112b47a189e11c9954671145253da5d
Arthur <arthur@utopios.solutions>
2023-06-26 10:59:44 +0200

    bases js

---
+++ demos/01-bases/main.js
@@ -0,0 +1,38 @@
+// Pour l'utilisation du JavaScript moderne
+'use strict';
+
+alert("Hello world!");
+
+// Commentaire sur une ligne
+
+/*
+
+Commentaire
+
+Multi-lignes
+
+*/
+
+// String
+let prenom = "Arthur";
+
+// Integer
+let age = 25;
+
+// Boolean
+let isAdulte = true;
+
+// JavaScript est case sensitive
+let AGE = 40;
+
+// Permet d'afficher des éléments dans la console
+console.log("age: ", age, " AGE: ", AGE);
+
+// Constantes
+
+const nom = "DENNETIERE";
+
+const YELLOW_SUBMARINE = "#ffff14";
+
+// nom = "TOTO"; ERREUR impossible de réassigner une constante
+
